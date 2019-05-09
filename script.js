99ee77864771f55a7fffded7f16aa0cb4ceee62c
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-09 18:20:57 +0200

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1- Traverse the DOM until you reach the children of the section element. This is an HTMLCollection which has a length property. Log it to the console. Does it match the number of elements inside the section element?
+
+// 2- Traverse the DOM until you reach the h3 heading. Save it to a variable. Change the innerText of the h3 to be something else.
+
+// 3- What is the difference between a HTMLCollection and a NodeList?
+
+// 4- How many ways do you know to capture the h1 element?
+
+// 5- Use the setAttribute method of elements to slap some inline styles to the h1.
+
+// 6- Use the setAttribute method of elements to slap a class of "cursive" to an element. Does it work?
+
+// 7- Use the style property of elements to slap some more inline styles to an element.
